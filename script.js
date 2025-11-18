d07b461b04222ce56f6c483e50d54614b02d9450
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-11-17 22:31:06 -0800

    Add number functionality and start operator func
    Add responsive buttons, which look like they are being clicked, and
    update the screen when they are clicked. Also started to work on
    operator button functionality, namely plus and equals. For whatever
    reason, the operate function works in the console, but when returning
    a result to the screen, it results in concatenation instead of
    summation. (9+8=98, instead of 17). That's my next bug to work out,
    tomorrow.

--- script.js
+++ script.js
@@ -2,6 +2,85 @@ let argA;
 let argB;
 let op;
 
+const screen = document.querySelector("#screen");
+screen.textContent = "";
+
+const nine = document.querySelector("#nine");
+nine.addEventListener("click", (event) => {
+    screen.textContent += 9;
+});
+
+const eight = document.querySelector("#eight");
+eight.addEventListener("click", (event) => {
+    screen.textContent += 8;
+});
+
+const seven = document.querySelector("#seven");
+seven.addEventListener("click", (event) => {
+    screen.textContent += 7;
+});
+
+const six = document.querySelector("#six");
+six.addEventListener("click", (event) => {
+    screen.textContent += 6;
+});
+
+const five = document.querySelector("#five");
+five.addEventListener("click", (event) => {
+    screen.textContent += 5;
+});
+
+const four = document.querySelector("#four");
+four.addEventListener("click", (event) => {
+    screen.textContent += 4;
+});
+
+const three = document.querySelector("#three");
+three.addEventListener("click", (event) => {
+    screen.textContent += 3;
+});
+
+const two = document.querySelector("#two");
+two.addEventListener("click", (event) => {
+    screen.textContent += 2;
+});
+
+const one = document.querySelector("#one");
+one.addEventListener("click", (event) => {
+    screen.textContent += 1;
+});
+
+const zero = document.querySelector("#zero");
+zero.addEventListener("click", (event) => {
+    screen.textContent += 0;
+});
+
+const ac = document.querySelector("#ac-btn");
+ac.addEventListener("click", (event) => {
+    screen.textContent = "";
+})
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", (event) => {
+    argB = screen.textContent;
+    argA = operate(argA, argB, op);
+    screen.textContent = argA;
+})
+
+const plus = document.querySelector("#plus");
+plus.addEventListener("click", (event) => {
+    // screen.textContent = operate(argA, argB, op);
+    argA = screen.textContent;
+    console.log(argA);
+    screen.textContent = "";
+    op = "+";
+})
+
+const minus = document.querySelector("#minus");
+minus.addEventListener("click", (event) => {
+
+})
+
 function add(a, b) {
     return a + b;
 }
@@ -23,4 +102,13 @@ function operate(a, b, op) {
     if (op == '-') return subtract (a, b);
     if (op == '*') return multiply (a, b);
     if (op == '/') return divide (a, b);
+}
+
+function input(a) {
+    screen.textContent = a;
+}
+
+function calc() {
+    argA = screen.textContent;
+    console.log(argA);
 }
