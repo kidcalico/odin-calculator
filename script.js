bbdb9cb36d236d422cf6b18f9c9e186e44c627af
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-11-19 11:39:16 -0800

    Fix decLimit to round decimals to the places

--- script.js
+++ script.js
@@ -45,10 +45,10 @@ zero.addEventListener('click', () => input('0'));
 
 const ac = document.querySelector('#ac-btn');
 ac.addEventListener('click', () => {
-    screen.textContent = '';
     argA = '';
     argB = '';
     op = '';
+    screen.textContent = '';
 });
 
 const backspace = document.querySelector('#backspace');
@@ -113,80 +113,101 @@ function operate(a, b, op) {
 // Function which limits numbers displayed
 function scrLimit() {
     const scrContent = argA + op + argB;
-    console.log(scrContent);
+    console.log("argA: " + argA + " op: " + op + " argB: " + argB);
+    console.log("scrcont: " + typeof scrContent)
+    screen.textContent = scrContent;
     if(scrContent.length > 11) {
         screen.textContent = scrContent.slice(0, 11);
     }
 }
 
 // Function which limits decimal places
-// function decLimit() {
-//     if(argA.includes('.')) {
-//         argA = Number.parseFloat(argA).toFixed(3);
-//     } 
-//     if (argB.includes('.')) {
-//         argB = argB.toFixed(3);
+function decLimit() {
+    argA = argA.toString();
+    argB = argB.toString();
+    if(argA.includes('.') && ((argA.indexOf('.') - argA.length) <= -3)) {
+        argA = Math.round(parseFloat(argA) * 1000) / 1000;
+        // argA = Number.parseFloat(argA).toFixed(3);
+    } 
+    if (argB.includes('.') && ((argB.indexOf('.') - argB.length) <= -3)) {
+        argB = Math.round(parseFloat(argB) * 1000) / 1000;
+        console.log(argB);
+        // argB = Number.parseFloat(argB).toFixed(3);
+    }
+}
 
-//     }
-// }
+// function which refreshes screen content
+function screenRefresh() {
+
+}
 
 // Function which handles input 'a'
 function input(a) {
-    if(numKey.includes(a) && op == '' && argB =='') {
+    if(numKey.includes(a) && op == '' && argB == '') {
         argA += a;
-        screen.textContent = argA;
+        console.log(typeof argA)
+        // screen.textContent = argA;
     } else if(numKey.includes(a) && op != '') {
         argB += a;
-        screen.textContent = argA + op + argB;
+        // screen.textContent = argA + op + argB;
     } else if(opKey.includes(a) && argB == '' && argA != '') {
         op = a;
-        screen.textContent = argA + op;
+        // screen.textContent = argA + op;
     } else if(a === '=') {
         argA = operate(argA, argB, op);
         argB = '';
         op = '';
-        screen.textContent = argA;
+        // screen.textContent = argA;
     } else if(opKey.includes(a) && op != '') {
         argA = operate(argA, argB, op);
         argB = '';
         op = a;
-        screen.textContent = argA + op;
+        // screen.textContent = argA + op;
     } else if(a === '±') {
         if(argB != '') {
             argB = parseFloat(argB) * (-1);
-            screen.textContent = argA + op + argB;
+            // screen.textContent = argA + op + argB;
         } else if(argA != '') {
             argA = parseFloat(argA) * (-1);
-            screen.textContent = argA;
+            // screen.textContent = argA;
         }
     } else if(a === '.') {
         if(argB != '' && !argB.includes('.')) {
             argB += '.';
-            screen.textContent = argA + op + argB;
+            // screen.textContent = argA + op + argB;
         } else if(argA != '' && argB == '' && !argA.includes('.')) {
             argA += '.';
-            screen.textContent = argA;
+            // screen.textContent = argA;
         } else if(argA == '') {
             argA += '0.';
-            screen.textContent = argA;
+            // screen.textContent = argA;
         }
     } else if(a === '%') {
         if(argB != '') {
             argB = makePercent(argB);
-            screen.textContent = argA + op + argB;
+            // screen.textContent = argA + op + argB;
         } else if (argA != '') {
             argA = makePercent(argA);
-            screen.textContent = argA;
+            // screen.textContent = argA;
         }
     } else if(a === '⬅︎') {
         if(argB != '') {
             argB = argB.slice(0, -1);
-            screen.textContent = argA + op + argB;
+            // screen.textContent = argA + op + argB;
         } else if (argA != '') {
             argA = argA.slice(0, -1);
-            screen.textContent = argA;
+            // screen.textContent = argA;
         }
     }
-    // decLimit();
+    decLimit();
+
+    // if (argB != '') {
+    //     screen.textContent = argA + op + argB;
+    // } else if (op != '') {
+    //     screen.textContent = argA + op;
+    // } else if (argA != '') {
+    //     screen.textContent = argA;
+    // }
+
     scrLimit();
 }
